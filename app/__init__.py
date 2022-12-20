dae757e23ba2c38f3f63ae77a10db612c18401df
Pachara Bunjong <flukexp@Pacharas-MacBook-Air.local>
2022-12-20 15:18:32 +0700

    Lab05

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
 app = Flask(__name__, static_folder='static')
 app.config['DEBUG'] = True
+app.config['SECRET_KEY']='10284942168ee2849a5c12888fd3f5ee1244f8f7590b763c'
 from app import views # noqa
