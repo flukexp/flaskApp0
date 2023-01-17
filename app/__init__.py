844b9a6b0e22ee9ea1fc2be320d4c777b371495d
flukexp <user_name@5cf9dde14a8d.localdomain>
2023-01-17 15:07:15 +0700

    Lab09

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
+
 from flask import Flask
 app = Flask(__name__, static_folder='static')
+app.config['JSON_AS_ASCII'] = False
 app.config['DEBUG'] = True
 app.config['SECRET_KEY']='10284942168ee2849a5c12888fd3f5ee1244f8f7590b763c'
 from app import views # noqa
